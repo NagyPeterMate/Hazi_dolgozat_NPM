6e5de3d29df39132f19fa664ee5befba72af6847
unknown <nagypetermate123@gmail.com>
2022-10-31 11:17:12 +0100

    controller létrehozása, migráció mappa ürítése

---
+++ app/Http/Controllers/Adatok.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Adatok extends Controller
+{
+    public function adatok(){
+        return view("adatok");
+    }
+}
