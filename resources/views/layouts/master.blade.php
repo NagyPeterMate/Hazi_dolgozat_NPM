c7cfec79caf788cdb3fde4a544347fe91bbedc6f
unknown <nagypetermate123@gmail.com>
2022-10-31 11:41:45 +0100

    bootstrap csatolás

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+
     @yield('content')
 </body>
 </html>
